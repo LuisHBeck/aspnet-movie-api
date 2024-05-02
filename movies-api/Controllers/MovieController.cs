d97baf668853ec87d969cb566ab631f2240f4c2a
LuisHBeck <beck.henrique.luis@gmail.com>
2024-05-02 16:20:35 -0300

    feat: first commit

---
+++ movies-api/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using movies_api.Models;
+
+namespace movies_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+    private static int id = 1;
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie) {
+        movie.Id = id++;
+        movies.Add(movie);
+    }
+
+    [HttpGet]
+    public IEnumerable<Movie> GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 10) {  
+        return movies.Skip(skip).Take(take); 
+    }
+
+    [HttpGet("{id}")]
+    public Movie? GetMovieById(int id)
+    {
+        return movies.FirstOrDefault(movie => movie.Id == id);
+    }
+}
---
+++ movies-api/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movies_api.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title field is required")]
+    public string Title { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "gender field can't exceed 50 characters")]
+    public string Gender { get; set; }
+
+    [Required]
+    [Range(45,600)]
+    public int Duration { get; set; }
+}
